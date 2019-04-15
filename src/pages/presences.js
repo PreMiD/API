f7f2356c4415fc7fc7847386bd1a7b0ab06fa2a3
Timeraa <29104008+Timeraa@users.noreply.github.com>
2019-04-15 15:25:20 +0200

    ⚙️ Update /presences

--- src/pages/presences.js
+++ src/pages/presences.js
@@ -3,7 +3,20 @@ var { query } = require('../database/functions');
 
 async function get(req, res) {
 	res.setHeader('Content-Type', 'application/json');
-	res.send(JSON.stringify((await query('SELECT url, name FROM presences')).rows));
+
+	if(!req.params.presence == undefined) {
+		res.send(JSON.stringify((await query('SELECT url, name FROM presences')).rows));
+		return
+	}
+	
+	var rows = (await query('SELECT url, name FROM presences WHERE name = ?', req.params.presence)).rows
+	
+	if(rows.length == 0) {
+		res.send(JSON.stringify({ error: 406, message: 'Presence not found.' }));
+		return
+	}
+
+	res.send(JSON.stringify(rows[0]))
 }
 
 //* Export function
