ad448e0c0d0ef71666609bc4e2e91ed7bbb59abb
Timeraa <29104008+Timeraa@users.noreply.github.com>
2019-05-14 20:53:55 +0200

    ⚙️ Mac AutoUpdater

--- src/pages/appUpdate.js
+++ src/pages/appUpdate.js
@@ -1,10 +1,14 @@
 async function get(req, res) {
-	if (req.params.os == 'windows.exe') {
-		res.download('./update.exe');
-		return;
-	}
+  if (req.params.os == "windows.exe") {
+    res.download("./update.exe");
+    return;
+  }
+  if (req.params.os == "macos.app") {
+    res.download("./macos.app");
+    return;
+  }
 
-	res.setHeader('Content-Type', 'text/xml');
-	res.send(await require('fs').readFileSync('./updateApp.xml'));
+  res.setHeader("Content-Type", "text/xml");
+  res.send(await require("fs").readFileSync("./updateApp.xml"));
 }
 module.exports = get;
