340e8f5d0b810e4b4a41e768762dbf717bb31669
Timeraa <29104008+Timeraa@users.noreply.github.com>
2019-05-25 02:08:50 +0200

    ⚙️ Add langFile/list

--- src/pages/langFile.js
+++ src/pages/langFile.js
@@ -1,20 +1,28 @@
 //* Require stuff
-var fs = require('fs');
+var fs = require("fs");
 
 async function get(req, res) {
-	if (req.params.langCode == undefined) {
-		res.send(JSON.stringify({ error: 405, message: 'No language code provided.' }));
-		return;
-	}
+  res.setHeader("Content-Type", "application/json");
+  if (req.params.langCode == undefined) {
+    res.send(
+      JSON.stringify({ error: 405, message: "No language code provided." })
+    );
+    return;
+  } else if (req.params.langCode.toLowerCase() == "list") {
+    res.send(
+      (await fs.readdirSync("./languages/")).map(l => l.split(".json")[0])
+    );
+  }
 
-	if (!await fs.existsSync(`./languages/${req.params.langCode}.json`)) {
-		res.send(JSON.stringify({ error: 408, message: 'Language file not found.' }));
-		return;
-	}
+  if (!(await fs.existsSync(`./languages/${req.params.langCode}.json`))) {
+    res.send(
+      JSON.stringify({ error: 408, message: "Language file not found." })
+    );
+    return;
+  }
 
-	//* Set headers
-	res.setHeader('Content-Type', 'application/json');
-	res.send(await fs.readFileSync(`./languages/${req.params.langCode}.json`));
+  //* Set headers
+  res.send(await fs.readFileSync(`./languages/${req.params.langCode}.json`));
 }
 
 //* Export function
