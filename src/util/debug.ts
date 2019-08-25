af8f69e2136fb87ee4488585d9807c1a78fd6d1d
Florian Metz <metz.florian@arcor.de>
2019-08-25 04:44:26 +0200

    ♻️ Debug info is always great lol

--- src/util/debug.ts
+++ src/util/debug.ts
@@ -1,8 +1,6 @@
 import chalk from "chalk";
 
 export function info(message) {
-  if (process.env.NODE_ENV != "dev") return;
-
   console.log(
     `${chalk.bgHex("#596cae")(chalk.bold(" API "))} ${chalk.hex("#5050ff")(
       message
@@ -11,8 +9,6 @@ export function info(message) {
 }
 
 export function success(message) {
-  if (process.env.NODE_ENV != "dev") return;
-
   var chalk = require("chalk");
   console.log(
     `${chalk.bgHex("#596cae")(chalk.bold(" API "))} ${chalk.hex("#50ff50")(
@@ -22,8 +18,6 @@ export function success(message) {
 }
 
 export function error(message) {
-  if (process.env.NODE_ENV != "dev") return;
-
   var chalk = require("chalk");
   console.log(
     `${chalk.bgHex("#596cae")(chalk.bold(" API "))} ${chalk.hex("#ff5050")(
