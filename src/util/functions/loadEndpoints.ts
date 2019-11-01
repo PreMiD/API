08507ec538739035dd26038c968d26368ab7e3a9
Florian Metz <metz.florian@arcor.de>
2019-11-01 03:44:11 +0100

    oof

--- src/util/functions/loadEndpoints.ts
+++ src/util/functions/loadEndpoints.ts
@@ -5,12 +5,12 @@ export default function loadEndpoints(server: express.Express, endpoints) {
   endpointVersions.map(eV => {
     endpoints[eV].map(e => {
       if (typeof e.path === "string") {
-        import(`./endpoints/${eV}/${e.handler}`)
+        import(`../../endpoints/${eV}/${e.handler}`)
           .then(module => server.get(e.path, module.handler))
           .catch(() => {});
       } else
         e.path.map((path: string) => {
-          import(`./endpoints/${eV}/${e.handler}`)
+          import(`../../endpoints/${eV}/${e.handler}`)
             .then(module => server.get(path, module.handler))
             .catch(() => {});
         });
