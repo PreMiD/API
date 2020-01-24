6b727c570d96d7e1de6c25dd05961ee1979bf2ab
Florian Metz <metz.florian@arcor.de>
2020-01-24 20:56:32 +0100

    ♻ Refactor for new installer

--- src/endpoints/default/appUpdate.ts
+++ src/endpoints/default/appUpdate.ts
@@ -4,24 +4,17 @@ import { readFileSync } from "fs";
 
 const versions = pmdDB.collection("versions");
 const handler: RequestHandler = async (req, res) => {
-  if (!req.params["bit"]) {
-    res.sendStatus(404);
-    return;
-  }
+	const appVersion: string = (
+		await versions.findOne({}, { projection: { _id: false, app: true } })
+	).app;
 
-  const appVersion: string = (await versions.findOne(
-    {},
-    { projection: { _id: false, app: true } }
-  )).app;
+	let xml = readFileSync("endpoints/default/data/appUpdate.xml", "utf-8");
+	xml = xml
+		.replace("VERSIONID", appVersion.replace(/[.]/g, "").padStart(4, "0"))
+		.replace("VERSION", appVersion);
 
-  let xml = readFileSync("endpoints/default/data/appUpdate.xml", "utf-8");
-  xml = xml
-    .replace("VERSIONID", appVersion.replace(/[.]/g, "").padStart(4, "0"))
-    .replace("VERSION", appVersion)
-    .replace("BIT", req.params["bit"]);
-
-  res.setHeader("Content-Type", "text/xml");
-  res.send(xml);
+	res.setHeader("Content-Type", "text/xml");
+	res.send(xml);
 };
 
 export { handler };
