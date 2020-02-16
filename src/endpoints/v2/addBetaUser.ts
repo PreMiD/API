2e42c680c4009cfcb53d1c5c54a87e172b8a516c
TheDrop <35700745+TheDropX@users.noreply.github.com>
2020-02-16 15:53:12 +0200

    :fire: Added more verification.

--- src/endpoints/v2/addBetaUser.ts
+++ src/endpoints/v2/addBetaUser.ts
@@ -3,6 +3,7 @@ import { pmdDB } from "../../db/client";
 
 //* Define credits collection
 const betaUsers = pmdDB.collection("betaUsers");
+const discordUsers = pmdDB.collection("discordUsers");
 
 //* Request Handler
 const handler: RequestHandler = async (req, res) => {
@@ -16,15 +17,27 @@ const handler: RequestHandler = async (req, res) => {
 
 	//* Find user in db
 	//* Send response
-	const user = await betaUsers.findOne(
-		{ userId: req.params["userId"] }
-	);
+	const user = await betaUsers.findOne({ userId: req.params["userId"] });
+	const discordUser = await discordUsers.findOne({ userId: req.params["userId"] });
 
-	if (user) {
-		res.send({ error: 2, message: "User already has beta access." });
+	let betaUsersCount = await betaUsers.countDocuments();
+
+	if (betaUsersCount < 200) {
+
+		if (user) {
+			res.send({ error: 2, message: "User already has beta access." });
+			return;
+		} else if (!user && discordUser) {
+			await betaUsers.insertOne({ userId: req.params["userId"] }).then(() => res.sendStatus(200));
+		} else if (!user && !discordUser) {
+			res.send({ error: 3, message: "User not in Discord Server." });
+			return;
+		}
+
+	} else {
+		res.send({ error: 4, message: "No more beta access slots available." });
 		return;
-	} else await betaUsers.insertOne({ userId: req.params["userId"] }).then(() => res.sendStatus(200));
-
+	}
 };
 
 //* Export handler
