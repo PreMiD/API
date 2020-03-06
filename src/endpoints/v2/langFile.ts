b406f0ceb9c8eba35ae9c92e0d2947e1d8bcf22b
Abdulbaki Dursun <eggsydev@outlook.com>
2020-03-06 18:35:44 +0300

    fix /list instead

--- src/endpoints/v2/langFile.ts
+++ src/endpoints/v2/langFile.ts
@@ -8,7 +8,30 @@ cache.onUpdate("langFiles", data => (langFiles = prepareLangFiles(data)));
 const handler: RequestHandler = (req, res) => {
   if (req.path.endsWith("/list")) {
     res.send(
-      langFiles.filter(lF => lF.project === "extension").map(lF => lF.lang)
+      langFiles
+        .filter(lF => lF.project === "extension")
+        .map(lF => {
+          let lang = lF.lang;
+
+          switch (lang.toLowerCase()) {
+            case "ja":
+              lF.lang = "ja_JP";
+              break;
+            case "zh-cn":
+              lF.lang = "zh_CN";
+              break;
+            case "zh-tw":
+              lF.lang = "zh_TW";
+              break;
+            case "ko":
+              lF.lang = "ko_KR";
+              break;
+            default:
+              break;
+          }
+
+          return lang;
+        })
     );
     return;
   }
@@ -49,42 +72,25 @@ const handler: RequestHandler = (req, res) => {
 
 function prepareLangFiles(langFiles) {
   langFiles.map(lF => {
-    if (lF.project == "extension") {
-      switch (lF.lang) {
-        case "ja_JP":
-          lF.lang = "ja";
-          break;
-        case "zh_CN":
-          lF.lang = "zh-CN";
-          break;
-        case "zh_TW":
-          lF.lang = "zh-TW";
-          break;
-        case "zh_HK":
-          lF.lang = "zh-TW";
-          break;
-        case "ko_KR":
-          lF.lang = "ko";
-          break;
-      }
-    } else if (lF.project == "website") {
-      switch (lF.lang.toLowerCase()) {
-        case "ja":
-          lF.lang = "ja_JP";
-          break;
-        case "zh-cn":
-          lF.lang = "zh_CN";
-          break;
-        case "zh-tw":
-          lF.lang = "zh_TW";
-          break;
-        case "ko":
-          lF.lang = "ko_KR";
-          break;
-        default:
-          break;
-      }
-    } else return;
+    if (lF.project !== "extension") return;
+
+    switch (lF.lang) {
+      case "ja_JP":
+        lF.lang = "ja";
+        break;
+      case "zh_CN":
+        lF.lang = "zh-CN";
+        break;
+      case "zh_TW":
+        lF.lang = "zh-TW";
+        break;
+      case "zh_HK":
+        lF.lang = "zh-TW";
+        break;
+      case "ko_KR":
+        lF.lang = "ko";
+        break;
+    }
   });
   return langFiles;
 }
