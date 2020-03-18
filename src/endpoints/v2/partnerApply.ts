25b457ebbb6d68f4af23955aaf34fa5de675af65
Florian Metz <me@timeraa.dev>
2020-03-18 04:41:52 +0100

    ♻ Recode + Spam protection

--- src/endpoints/v2/partnerApply.ts
+++ src/endpoints/v2/partnerApply.ts
@@ -1,44 +1,92 @@
 import { RequestHandler } from "express";
-import axios from "axios";
+import { getDiscordUser } from "../../util/functions/getDiscordUser";
+import { pmdDB } from "../../db/client";
+import { WebhookClient } from "discord.js";
+
+const coll = pmdDB.collection("applications");
+const webhook = new WebhookClient(
+	process.env.DISCORD_WEBHOOK_ID,
+	process.env.DISCORD_WEBHOOK_TOKEN
+);
 
 //* Request Handler
 const handler: RequestHandler = async (req, res) => {
-	await axios
-		.post(
-			`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET}&response=${req.body.gresponse}`,
-			{
-				headers: { "Content-Type": "application/x-www-form-urlencoded" }
+	if (
+		!req.body.type ||
+		!req.body.name ||
+		!req.body.link ||
+		!req.body.description ||
+		!req.body.imageLink ||
+		!req.body.token
+	) {
+		res.send({ error: 1, message: "Missing fields." });
+		return;
+	}
+
+	getDiscordUser(req.body.token)
+		.then(async dUser => {
+			if (
+				await coll.findOne({
+					type: "partner",
+					userId: dUser.id,
+					reviewed: false
+				})
+			) {
+				res.send({ error: 3, message: "You already applied before." });
+				return;
 			}
-		)
-		.then(async data => {
-			if (data.data.success) {
-				await axios
-					.post(`${process.env.DISCORD_WEBHOOK}`, {
-						username: "Partner Applications",
-						embeds: [
+
+			res.sendStatus(200);
+
+			coll.insertOne({
+				type: "partner",
+				userId: dUser.id,
+				reviewed: false,
+				pType: req.body.type,
+				name: req.body.name,
+				link: req.body.link,
+				description: req.body.description,
+				imageLink: req.body.imageLink
+			});
+
+			webhook.send("", {
+				embeds: [
+					{
+						title: `Partner Application (${req.body.type})`,
+						description: `By <@${dUser.id}>`,
+						fields: [
 							{
-								title: "New partner application",
-								description: `**Name**: ${req.body.name}\n**Type**: ${req.body.type}\n**URL**: ${req.body.link}\n**Description:** ${req.body.description}\n\n ~ ${req.body.discordUser.userTag} / <@${req.body.discordUser.userId}>`,
-								thumbnail: {
-									url: req.body.imageLink
-								}
+								name: "Type",
+								value: req.body.type,
+								inline: false
+							},
+							{
+								name: "Name",
+								value: req.body.name,
+								inline: false
+							},
+							{
+								name: "URL",
+								value: req.body.link,
+								inline: false
+							},
+							{
+								name: "Description",
+								value: req.body.description,
+								inline: false
 							}
-						]
-					})
-					.then(function(response) {
-						res.sendStatus(200);
-					})
-					.catch(function(err) {
-						if (err)
-							res.send({ status: 500, message: "Error posting to Discord Webhook." });
-						console.log(err);
-					});
-			} else res.sendStatus(401);
+						],
+						thumbnail: {
+							url: `https://cdn.discordapp.com/avatars/${dUser.id}/${dUser.avatar}.png`
+						},
+						image: {
+							url: req.body.imageLink
+						}
+					}
+				]
+			});
 		})
-		.catch(err => {
-			if (err) res.send({ status: 500, message: "Error posting to Google." });
-			console.log(err);
-		});
+		.catch(() => res.send({ error: 2, message: "Invalid token." }));
 };
 
 //* Export handler
