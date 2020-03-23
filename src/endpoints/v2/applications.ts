972af39c7698ee0b841c617ef551236f254483e8
TheDrop <35700745+TheDropX@users.noreply.github.com>
2020-03-23 04:30:52 +0200

    :fire: Applications endpoint.

---
+++ src/endpoints/v2/applications.ts
@@ -0,0 +1,33 @@
+import { RequestHandler } from "express";
+import { pmdDB } from "../../db/client";
+import { getDiscordUser } from "../../util/functions/getDiscordUser";
+
+const coll = pmdDB.collection("applications");
+const credits = pmdDB.collection("credits");
+
+//* Request Handler
+const handler: RequestHandler = async (req, res) => {
+	if (!req.params["token"]) {
+		//* send error
+		//* return
+		res.send({ error: 1, message: "No token providen." });
+		return;
+	}
+
+	getDiscordUser(req.params["token"]).then(async dUser => {
+		if (
+			await credits.findOne({
+				userId: dUser.id,
+				roles: { $in: ["Staff Head"] }
+			})
+		) {
+			res.send(await coll.find().toArray());
+			return;
+		}
+  }).catch(err => {
+    res.sendStatus(401);
+  });
+};
+
+//* Export handler
+export { handler };
