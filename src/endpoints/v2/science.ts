319f3f99770ac8b78f801e9122fac8329dc088de
Florian Metz <me@timeraa.dev>
2020-03-27 21:26:58 +0100

    ✨ DELETE - science

--- src/endpoints/v2/science.ts
+++ src/endpoints/v2/science.ts
@@ -6,30 +6,45 @@ const science = pmdDB.collection("science");
 
 //* Request Handler
 const handler: RequestHandler = (req, res) => {
-	if (
-		!req.body.identifier ||
-		typeof req.body.identifier !== "string" ||
-		!req.body.presences ||
-		!Array.isArray(req.body.presences)
-	) {
-		res.sendStatus(404);
-		return;
-	}
+	if (req.method === "POST") {
+		if (
+			!req.body.identifier ||
+			typeof req.body.identifier !== "string" ||
+			!req.body.presences ||
+			!Array.isArray(req.body.presences)
+		) {
+			res.sendStatus(404);
+			return;
+		}
 
-	science
-		.findOneAndUpdate(
-			{ identifier: req.body.identifier },
-			{
-				$set: {
-					identifier: req.body.identifier,
-					presences: req.body.presences,
-					updated: Date.now()
-				}
-			},
-			{ upsert: true }
-		)
-		.then(() => res.sendStatus(200))
-		.catch(() => res.sendStatus(500));
+		science
+			.findOneAndUpdate(
+				{ identifier: req.body.identifier },
+				{
+					$set: {
+						identifier: req.body.identifier,
+						presences: req.body.presences,
+						updated: Date.now()
+					}
+				},
+				{ upsert: true }
+			)
+			.then(() => res.sendStatus(200))
+			.catch(() => res.sendStatus(500));
+	} else if (req.method === "DELETE") {
+		if (!req.body.identifier) {
+			res.sendStatus(404);
+			return;
+		}
+
+		science
+			.findOneAndDelete({ identifier: req.body.identifier })
+			.then(response => {
+				if (response.value) res.sendStatus(200);
+				else res.sendStatus(404);
+			})
+			.catch(() => res.sendStatus(500));
+	}
 };
 
 //* Export handler
