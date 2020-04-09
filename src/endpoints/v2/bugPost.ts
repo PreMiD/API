d7ea794eddc61c8cfe1dee61ac3b6c4bcfc87c03
The Gamerzs <thgamerzs74@gmail.com>
2020-04-09 23:19:55 +0100

    Bug Support

---
+++ src/endpoints/v2/bugPost.ts
@@ -0,0 +1,40 @@
+import { RequestHandler } from "express";
+import { pmdDB } from "../../db/client";
+
+const coll = pmdDB.collection("bugs");
+
+//* Request Handler
+const handler: RequestHandler = async (req, res) => {
+	if (!req.body.token) {
+		res.send({ error: 1, message: "No token providen." });
+		return;
+	}
+
+	if (!req.body.bug_brief) {
+		res.send({ error: 2, message: "No brief providen." });
+		return;
+    }
+    
+    if (!req.body.bug_description) {
+		res.send({ error: 2, message: "No description providen." });
+		return;
+	}
+
+    res.sendStatus(200);
+
+    const id = await coll.countDocuments();
+    console.log(id);
+
+    coll.insertOne({
+        bug_id: id+1,
+        bug_brief:req.body.bug_brief,
+        bug_description:req.body.bug_description,
+        bug_status: req.body.bug_status,
+        bug_date: req.body.bug_date,
+        bug_userName:req.body.bug_userName,
+        bug_userId:req.body.bug_userId
+    });
+};
+
+//* Export handler
+export { handler };
