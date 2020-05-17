134c470208a6b0405f0c5b3d5a8055b2d29c44c7
Abdulbaki Dursun <eggsydev@outlook.com>
2020-05-17 22:59:33 +0300

    :construction: WIP | Crowdin translation notifier.
    hope i get this working in one shot or yall gey

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import { config } from "dotenv";
 config({ path: "../.env" });
 
 import CacheManager from "./util/CacheManager";
+import { initialize as crowdinInitialize } from "./util/crowdinManager";
 import { master } from "./util/cluster/master";
 import { worker } from "./util/cluster/worker";
 import cluster from "cluster";
 
 export const cache = new CacheManager();
 
+crowdinInitialize();
 if (cluster.isMaster) master();
 else worker();
