d32238098c3edfe3c50bfa7123a2aec57afabfb5
Florian Metz <me@timeraa.dev>
2020-05-22 03:15:07 +0200

    ✨ Add limit & random args

--- src/endpoints/v3/fields/credits.ts
+++ src/endpoints/v3/fields/credits.ts
@@ -1,7 +1,7 @@
 import { cache } from "../../../index";
 import { creditsType } from "../types/credits/credits";
+import { GraphQLBoolean, GraphQLInt, GraphQLString } from "graphql";
 import { GraphQLList } from "graphql/type/definition";
-import { GraphQLString } from "graphql";
 
 let creditsCache = prepareCredits(cache.get("credits"));
 
@@ -10,11 +10,18 @@ cache.onUpdate("credits", data => (creditsCache = prepareCredits(data)));
 export const credits = {
 	type: GraphQLList(creditsType),
 	args: {
-		id: { type: GraphQLString, defaultValue: null }
+		id: { type: GraphQLString, defaultValue: null },
+		limit: { type: GraphQLInt, defaultValue: null },
+		random: { type: GraphQLBoolean, defaultValue: false }
 	},
-	resolve(_, args: { id?: string }) {
-		if (args.id) return creditsCache.filter(c => c.user.id === args.id);
-		else return creditsCache;
+	resolve(_, args: { id?: string; limit?: number; random: false }) {
+		let res = creditsCache;
+
+		if (args.id) res.filter(c => c.user.id == args.id);
+		if (args.random) res = shuffle(res);
+		if (args.limit) res = res.slice(0, args.limit);
+
+		return res;
 	}
 };
 
@@ -35,4 +42,21 @@ function prepareCredits(credits) {
 		},
 		roles: c.roles.map((r, i) => ({ id: c.roleIds[i], name: r }))
 	}));
+}
+
+function shuffle(array: Array<any>) {
+	var currentIndex = array.length,
+		temporaryValue: any,
+		randomIndex: number;
+
+	while (0 !== currentIndex) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
 }
