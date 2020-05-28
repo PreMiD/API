be8f9870387c5d1027f9e79577a236ab217ff6ae
Abdulbaki Dursun <eggsydev@outlook.com>
2020-05-28 15:37:55 +0300

    :wrench: Move crowdinManager

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,23 +1,23 @@
 import { MongoClient as mClient, Db } from "mongodb";
+import { initialize as CrowdinInitialize } from "../util/crowdinManager";
 
 export let pmdDB: Db = null;
 
-export const client = new mClient(
-	process.env.MONGO_URL,
-	{
-		appname: "PreMiD-API",
-		useUnifiedTopology: true
-	}
-);
+export const client = new mClient(process.env.MONGO_URL, {
+  appname: "PreMiD-API",
+  useUnifiedTopology: true,
+});
 
 export const connect = async () => {
-	return new Promise((resolve, reject) => {
-		client
-			.connect()
-			.then(mClient => {
-				resolve(mClient);
-				pmdDB = client.db("PreMiD");
-			})
-			.catch(reject);
-	});
+  return new Promise((resolve, reject) => {
+    client
+      .connect()
+      .then((mClient) => {
+        resolve(mClient);
+        pmdDB = client.db("PreMiD");
+
+        CrowdinInitialize();
+      })
+      .catch(reject);
+  });
 };
