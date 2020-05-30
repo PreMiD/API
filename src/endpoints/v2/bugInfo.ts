3e7bed828eb0a49865df79ba22324017ebf02ff4
The Gamerzs <thgamerzs74@gmail.com>
2020-05-30 21:57:07 +0100

    :recycle: Fixed issues

--- src/endpoints/v2/bugInfo.ts
+++ src/endpoints/v2/bugInfo.ts
@@ -1,40 +1,31 @@
 import { RequestHandler } from "express";
 import { cache } from "../../index";
 import { getDiscordUser } from "../../util/functions/getDiscordUser";
-import { log } from "util";
 
 //* Define credits collection
 let bug = cache.get("bugs");
-cache.onUpdate("bugs", data => (bug = data));
-let bugs = cache.get("bugUsers")
-cache.onUpdate("bugUsers", data => (bug = data));
-
-
+cache.onUpdate("bugs", (data) => (bug = data));
 
 //* Request Handler
 const handler: RequestHandler = async (req, res) => {
-	if (!req.params["token"]) {
-		//* send error
-		//* return
-		res.send({ error: 1, message: "No token providen." });
-		return;
-	}
+  if (!req.params["token"]) {
+    //* send error
+    //* return
+    res.send({ error: 1, message: "No token providen." });
+    return;
+  }
 
-
-
-	getDiscordUser(req.params["token"])
-		.then(async dUser => {
-
-			//* find user
-			//* Return found bugs
-			var result = new Array();
-			bug.filter(b => b.userId === dUser.id && b.status === "New")
-			if (bug) res.send(bug);
-			else res.send({ error: 2, message: "No bugs found." });
-		})
-		.catch(err => {
-			res.sendStatus(401);
-		});
+  getDiscordUser(req.params["token"])
+    .then(async (dUser) => {
+      //* find user
+      //* Return found bugs
+      bug.filter((b) => b.userId === dUser.id && b.status === "New");
+      if (bug) res.send(bug);
+      else res.send({ error: 2, message: "No bugs found." });
+    })
+    .catch((err) => {
+      res.sendStatus(401);
+    });
 };
 
 //* Export handler
--- src/endpoints/v2/bugUserInfo.ts
+++ src/endpoints/v2/bugUserInfo.ts
@@ -3,34 +3,31 @@ import { RequestHandler } from "express";
 import { getDiscordUser } from "../../util/functions/getDiscordUser";
 
 //* Define bugUserInfo collection
-let bug = cache.get("bugUsers")
-cache.onUpdate("bugUsers", data => (bug = data));
-let bugs = cache.get("bugs")
-cache.onUpdate("bugs", data => (bugs = data));
+let bug = cache.get("bugUsers");
+cache.onUpdate("bugUsers", (data) => (bug = data));
 
 //* Request Handler
 const handler: RequestHandler = async (req, res) => {
-	//* userId not providen
-	if (!req.params["token"]) {
-		//* send error
-		//* return
-		res.send({ error: 1, message: "No token providen." });
-		return;
-	}
+  //* userId not providen
+  if (!req.params["token"]) {
+    //* send error
+    //* return
+    res.send({ error: 1, message: "No token providen." });
+    return;
+  }
 
-	getDiscordUser(req.params["token"])
-		.then(async dUser => {
-			//* find user
-			//* Return user if found
-			//* Else return default 3
-			const info = bug.find(b => b.userId === dUser.id);
-			if (info === undefined || info === "" || !info) res.send(null);
-			else res.send(info);
-			
-		})
-		.catch(err => {
-			res.sendStatus(401);
-		});
+  getDiscordUser(req.params["token"])
+    .then(async (dUser) => {
+      //* find user
+      //* Return user if found
+      //* Else return default 3
+      const info = bug.find((b) => b.userId === dUser.id);
+      if (info === undefined || info === "" || !info) res.send(null);
+      else res.send(info);
+    })
+    .catch((err) => {
+      res.sendStatus(401);
+    });
 };
 
 //* Export handler
