0817796fd719f46f4f05d9d3906557c7e6bf2f58
Florian Metz <me@timeraa.dev>
2020-06-05 21:37:59 +0200

    ♻ organize file
    Co-authored-by: Ice <johnburke934@gmail.com>

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,23 +1,27 @@
-import { MongoClient as mClient, Db } from "mongodb";
 import { initialize as CrowdinInitialize } from "../util/crowdinManager";
+import { config } from "dotenv";
+import { Db, MongoClient as mClient } from "mongodb";
+import { join } from "path";
+
+config({ path: join(process.cwd(), "../.env") });
 
 export let pmdDB: Db = null;
 
 export const client = new mClient(process.env.MONGO_URL, {
-  appname: "PreMiD-API",
-  useUnifiedTopology: true,
+	appname: "PreMiD-API",
+	useUnifiedTopology: true
 });
 
 export const connect = async () => {
-  return new Promise((resolve, reject) => {
-    client
-      .connect()
-      .then((mClient) => {
-        resolve(mClient);
-        pmdDB = client.db("PreMiD");
+	return new Promise((resolve, reject) => {
+		client
+			.connect()
+			.then(mClient => {
+				resolve(mClient);
+				pmdDB = client.db("PreMiD");
 
-        CrowdinInitialize();
-      })
-      .catch(reject);
-  });
+				CrowdinInitialize();
+			})
+			.catch(reject);
+	});
 };
