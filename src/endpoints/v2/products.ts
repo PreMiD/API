c24d4a6c997c5ac8001fb47003d49852e2b1cb65
TheGamerzs <44209534+TheGamerzs@users.noreply.github.com>
2020-06-29 23:22:05 +0100

    Update products.ts

--- src/endpoints/v2/products.ts
+++ src/endpoints/v2/products.ts
@@ -5,11 +5,23 @@ let products = cache.get("merch");
 
 cache.on("update", (_, data) => (products = data));
 
-
+let types = [];
 
 //* Request Handler
 const handler: RequestHandler = async (req, res) => {
-    res.send("Hi")
+	products
+		.filter(p => p.title === "categories")[0]
+		["list"].forEach(category_name => {
+			types[category_name] = [];
+		});
+
+	products
+		.filter(p => p.category != undefined && p.status === "Live")
+		.forEach(product_info => {
+			types[product_info.category].push(product_info);
+		});
+
+	res.send(types);
 };
 
 //* Export handler
