8ebc0c451b526b3cc61d002a3203c72bddcabb60
Maja <mail@ririxi.dev>
2020-10-24 22:36:43 +0200

    🐛  fix

--- src/endpoints/v2/presenceUsage.ts
+++ src/endpoints/v2/presenceUsage.ts
@@ -21,7 +21,9 @@ export function prepareUsage(science) {
 	let ranking = {},
 		ranks = [];
 
-	science.forEach(s => ranks.push(s.presences));
+	for (let i = 0; i < science.presences.length; i++) {
+		ranks.push(science.presences[i]);
+	}
 
 	for (let i = 0; i < ranks.length; i++) {
 		ranking[ranks[i]] = (ranking[ranks[i]] || 0) + 1;
