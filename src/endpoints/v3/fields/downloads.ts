0cd945b140a0de765790bae136c522b6a79237ce
TheDropX <alex.tulbure4@gmail.com>
2020-12-04 22:24:44 +0200

    :sparkles: Updated downloads.

--- src/endpoints/v3/fields/downloads.ts
+++ src/endpoints/v3/fields/downloads.ts
@@ -8,41 +8,42 @@ import { downloadsType } from "../types/downloads/downloadsType";
 let downloadsCache = prepareDownloads(cache.get("downloads"));
 
 cache.on("update", (_, data) => (downloadsCache = prepareDownloads(data)), {
-	only: "downloads"
+	only: "downloads",
 });
 
 export const downloads = {
 	type: GraphQLList(downloadsType),
 	args: {
-		releaseType: { type: GraphQLString, defaultValue: null }
+		releaseType: { type: GraphQLString, defaultValue: null },
+		token: { type: GraphQLString, defaultValue: null },
 	},
-	async resolve(_, _1, context) {
-		if (context.query?.token) {
+	async resolve(_, args) {
+		if (args.token) {
 			try {
-				const dUser = await getDiscordUser(context.query.token),
+				const dUser = await getDiscordUser(args.token),
 					accessType = cache
 						.get("alphaUsers")
-						.find(aU => aU.userId === dUser.id)
+						.find((aU) => aU.userId === dUser.id)
 						? "alpha"
-						: cache.get("betaUsers").find(aU => aU.userId === dUser.id)
+						: cache.get("betaUsers").find((aU) => aU.userId === dUser.id)
 						? "beta"
 						: false;
 
 				if (accessType === "alpha") return downloadsCache;
 				else if (accessType === "beta")
-					return [downloadsCache.find(c => c.releaseType === "beta")];
+					return [downloadsCache.find((c) => c.releaseType === "beta")];
 				else return [];
 			} catch (e) {
 				return [];
 			}
 		} else return [];
-	}
+	},
 };
 
 function prepareDownloads(downloads) {
-	return downloads.map(c => ({
+	return downloads.map((c) => ({
 		releaseType: c.item,
 		appLinks: c.app_links,
-		extLinks: c.ext_links
+		extLinks: c.ext_links,
 	}));
 }
