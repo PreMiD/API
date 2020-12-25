0fd3b256002c3b72773829d57d70e6401e19244c
TheDropX <alex.tulbure4@gmail.com>
2020-12-25 17:10:11 +0200

    :sparkles: Nothing special.

--- src/endpoints/v3/types/downloads/downloadsType.ts
+++ src/endpoints/v3/types/downloads/downloadsType.ts
@@ -1,4 +1,4 @@
-import { GraphQLString } from "graphql";
+import { GraphQLBoolean, GraphQLString } from "graphql";
 import { GraphQLList, GraphQLObjectType } from "graphql/type/definition";
 
 import { downloadLinkType } from "./downloadLinkType";
@@ -7,6 +7,7 @@ export const downloadsType = new GraphQLObjectType({
 	name: "Downloads",
 	fields: () => ({
 		releaseType: { type: GraphQLString },
+		enabled: { type: GraphQLBoolean },
 		appLinks: { type: GraphQLList(GraphQLString) },
 		extLinks: { type: GraphQLList(downloadLinkType) }
 	})
