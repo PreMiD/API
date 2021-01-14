6db855cff367772b7e64d5de05a4fa7cd2dd5372
Florian Metz <me@timeraa.dev>
2021-01-14 18:00:49 +0100

    ✨ Allow array of services

--- src/endpoints/v3/fields/presences.ts
+++ src/endpoints/v3/fields/presences.ts
@@ -1,5 +1,5 @@
-import { GraphQLInt, GraphQLString } from "graphql";
-import { GraphQLList } from "graphql/type/definition";
+import { GraphQLError, GraphQLInt, GraphQLString } from "graphql";
+import { GraphQLList, GraphQLScalarType } from "graphql/type/definition";
 
 import { cache } from "../../../index";
 import { prepareUsage } from "../../v2/presenceUsage";
@@ -28,7 +28,12 @@ function preparePresences(cache) {
 export const presences = {
 	type: GraphQLList(presenceType),
 	args: {
-		service: { type: GraphQLString, defaultValue: null },
+		service: {
+			type: new GraphQLScalarType({
+				name: "StringOrStringArray"
+			}),
+			defaultValue: null
+		},
 		author: { type: GraphQLString, defaultValue: null },
 		contributor: { type: GraphQLString, defaultValue: null },
 		start: { type: GraphQLInt, defaultValue: null },
@@ -39,7 +44,7 @@ export const presences = {
 	resolve(
 		_,
 		args: {
-			service?: string;
+			service?: string | string[];
 			author?: string;
 			contributor?: string;
 			start?: number;
@@ -48,6 +53,18 @@ export const presences = {
 			tag?: string;
 		}
 	) {
+		if (
+			args.service &&
+			!(
+				typeof args.service === "string" ||
+				(Array.isArray(args.service) &&
+					args.service.every(v => typeof v === "string"))
+			)
+		)
+			throw new GraphQLError(
+				"Argument Service must be a string or an array of strings."
+			);
+
 		let result = presencesCache;
 
 		result = result.filter(p => {
@@ -55,7 +72,12 @@ export const presences = {
 				checksThatPassed = 0;
 
 			if (args.service) {
-				if (args.service === p.metadata.service) checksThatPassed++;
+				if (
+					typeof args.service === "string"
+						? args.service === p.metadata.service
+						: args.service.some(s => p.metadata.service === s)
+				)
+					checksThatPassed++;
 			} else checksThatPassed++;
 			if (args.author) {
 				if (args.author === p.metadata.author.id) checksThatPassed++;
