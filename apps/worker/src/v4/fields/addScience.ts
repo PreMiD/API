a72efa62edf9e38267a6223a437df4a732d4c055
Bas950 <me@bas950.com>
2023-11-10 18:22:32 +0100

    feat: add heartbeat

---
+++ apps/worker/src/v4/fields/addScience.ts
@@ -0,0 +1,51 @@
+import { gql, UserInputError } from "apollo-server-core";
+import validator from "validator";
+
+import { redis } from "../..";
+
+export const schema = gql`
+	type Mutation {
+		addScience(
+			identifier: String!
+			presences: [String]
+			os: String
+			arch: String
+		): AddScienceResult
+	}
+
+	type AddScienceResult {
+		identifier: String
+		presences: [String]
+		os: String
+		arch: String
+	}
+`;
+
+export async function resolver(
+	_: any,
+	params: { identifier: string; presences: string[]; os: string; arch: string }
+) {
+	if (!validator.isUUID(params.identifier, "4"))
+		return new UserInputError("identifier must be a UUID v4.");
+
+	const data = {
+		identifier: params.identifier,
+		presences: params.presences.filter(p => p.trim().length),
+		platform: {
+			os: params.os,
+			arch: params.arch
+		}
+	};
+
+	await redis.hset(
+		"pmd-api.scienceUpdates",
+		params.identifier,
+		JSON.stringify(data)
+	);
+
+	return data;
+}
+
+export const options = {
+	type: "mutation"
+};
---
+++ apps/worker/src/v4/fields/heartbeat.ts
@@ -0,0 +1,93 @@
+import { gql, UserInputError } from "apollo-server-core";
+import validator from "validator";
+
+import { redis } from "../..";
+
+export const schema = gql`
+	type Mutation {
+		heartbeat(
+			identifier: String!
+			presence: HeartbeatPresenceInput
+			extension: HeartbeatExtensionInput!
+		): HeartbeatResult
+	}
+
+	input HeartbeatPresenceInput {
+		service: String!
+		verion: String!
+		language: String!
+		since: Float!
+	}
+
+	input HeartbeatExtensionInput {
+		version: String!
+		language: String!
+		connected: HeartbeatConnectedInput
+	}
+
+	input HeartbeatConnectedInput {
+		app: Int!
+		discord: Boolean!
+	}
+
+	type HeartbeatResult {
+		identifier: String!
+		presence: HeartbeatPresence
+		extension: HeartbeatExtension!
+	}
+
+	type HeartbeatPresence {
+		service: String!
+		verion: String!
+		language: String!
+		since: Float!
+	}
+
+	type HeartbeatExtension {
+		version: String!
+		language: String!
+		connected: HeartbeatConnected
+	}
+
+	type HeartbeatConnected {
+		app: Int!
+		discord: Boolean!
+	}
+`;
+
+export async function resolver(
+	_: any,
+	params: {
+		identifier: string;
+		presence?: {
+			service: string;
+			version: string;
+			language: string;
+			since: number;
+		};
+		extension: {
+			version: string;
+			language: string;
+			connected?: {
+				app: number;
+				discord: boolean;
+			};
+		};
+	}
+) {
+	if (!validator.isUUID(params.identifier, "4"))
+		return new UserInputError("identifier must be a UUID v4.");
+
+	await redis.setex(
+		`pmd-api.heartbeatUpdates.${params.identifier}`,
+		// 5 minutes
+		300,
+		JSON.stringify(params)
+	);
+
+	return params;
+}
+
+export const options = {
+	type: "mutation"
+};
