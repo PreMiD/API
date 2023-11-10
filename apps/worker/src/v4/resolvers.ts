1fcbfc17e5dfc14cba63cf7b387650d23aa48229
Bas950 <me@bas950.com>
2023-11-10 19:18:13 +0100

    fix: add mutations

--- apps/worker/src/v4/resolvers.ts
+++ apps/worker/src/v4/resolvers.ts
@@ -9,12 +9,12 @@ export const resolvers = new Promise<any>(async r => {
 			...(await getFields("query", "v4/fields")).map(r1 => ({
 				[r1[0]]: r1[1].resolver
 			}))
-		) /*
+		),
 		Mutation: Object.assign(
 			{},
 			...(await getFields("mutation", "v4/fields")).map(r1 => ({
 				[r1[0]]: r1[1].resolver
 			}))
-		) */
+		)
 	});
 });
